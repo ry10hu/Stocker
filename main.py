f930bbfc18aba2148765dee33d7a6e982c863c44
ry10hu <115399801+ry10hu@users.noreply.github.com>
2024-11-16 23:09:44 +0000

    Added main.py + error handling.

---
+++ main.py
@@ -0,0 +1,30 @@
+import json
+from commands import *
+
+commands_dict = {"buy": buy,
+                 "clear": clear,
+                 "inventory": inventory,
+                 "save": save,
+                 "sell": sell
+                 }
+
+while True:
+    user_input = input("Input: ")
+
+    user_input_split = user_input.split(" ")
+    try:
+        if len(user_input_split) == 1:
+            commands_dict[user_input_split[0]]()
+        
+        elif len(user_input_split) == 2:
+            arg1 = user_input_split[1]
+            commands_dict[user_input_split[0]](arg1)
+        
+        elif len(user_input_split) == 3:
+            arg1 = user_input_split[1]
+            arg2 = user_input_split[2]
+            commands_dict[user_input_split[0]](arg1, arg2)
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print(message)
