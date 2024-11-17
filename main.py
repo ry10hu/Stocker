e27e2cba642ff29733af8a1a4c28ca9c8d27b3cc
ry10hu <115399801+ry10hu@users.noreply.github.com>
2024-11-17 00:37:15 +0000

    wrapped code in function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import json
 from commands import *
 
@@ -7,24 +9,25 @@ commands_dict = {"buy": buy,
                  "save": save,
                  "sell": sell
                  }
+def user_input():
+    while True:
+        user_input = input("Input: ")
 
-while True:
-    user_input = input("Input: ")
+        user_input_split = user_input.split(" ")
+        try:
+            if len(user_input_split) == 1:
+                commands_dict[user_input_split[0]]()
+            
+            elif len(user_input_split) == 2:
+                arg1 = user_input_split[1]
+                commands_dict[user_input_split[0]](arg1)
+            
+            elif len(user_input_split) == 3:
+                arg1 = user_input_split[1]
+                arg2 = user_input_split[2]
+                commands_dict[user_input_split[0]](arg1, arg2)
 
-    user_input_split = user_input.split(" ")
-    try:
-        if len(user_input_split) == 1:
-            commands_dict[user_input_split[0]]()
-        
-        elif len(user_input_split) == 2:
-            arg1 = user_input_split[1]
-            commands_dict[user_input_split[0]](arg1)
-        
-        elif len(user_input_split) == 3:
-            arg1 = user_input_split[1]
-            arg2 = user_input_split[2]
-            commands_dict[user_input_split[0]](arg1, arg2)
-    except Exception as ex:
-        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-        message = template.format(type(ex).__name__, ex.args)
-        print(message)
+        except Exception as ex:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
