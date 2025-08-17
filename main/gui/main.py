cd01d19f525ff77c7b2393539b698aefeba8c788
ry10hu <ry10hu@gmail.com>
2025-08-16 22:53:43 -0700

    Update main.py
    reformat and some new buttons, not finished yet

--- main/gui/main.py
+++ main/gui/main.py
@@ -3,6 +3,8 @@ import tkinter.ttk as ttk
 from gui.AssetTab import AssetTab  # your tab frame class
 from gui.MainMenuTab import MainMenuTab
 import gui.shortcuts as shortcuts
+import logic.commands as commands
+
 
 class MainApp(tk.Tk):
     def __init__(self):
@@ -13,25 +15,23 @@ class MainApp(tk.Tk):
         self.title("Main Window")
 
         self.style = ttk.Style()
-        self.style.theme_use('default')
+        self.style.theme_use("default")
 
         self.notebook = ttk.Notebook(self)
         self.notebook.place(relx=0.0, rely=0.0, relheight=1.0, relwidth=1.0)
 
-        mainmenutab = MainMenuTab(self.notebook, notebook = self.notebook)
-        self.notebook.add(mainmenutab, text='Main Menu')
-        
+        mainmenutab = MainMenuTab(self.notebook, notebook=self.notebook)
+        self.notebook.add(mainmenutab, text="Main Menu")
+
         assettab = AssetTab(self.notebook)
-        
+
         shortcuts.bind_shortcuts(self, self.notebook)
-        
 
 
 def start_up():
     app = MainApp()
     app.mainloop()
 
+
 if __name__ == "__main__":
     start_up()
-
-
